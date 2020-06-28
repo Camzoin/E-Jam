642320c36e7e64dce509230a0a882bca05a95176
Camzoin <Camden.Cecrle@gmail.com>
2020-06-27 17:47:43 -0700

    Basement update

---
+++ Assets/ShaderController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    public float saturation = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalFloat("AlbedoSaturation", saturation);
+        Shader.SetGlobalFloat("LightingSaturation", saturation);
+    }
+}
---
+++ Assets/leaveEarly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leaveEarly : MonoBehaviour
+{
+    GameObject sceneChanger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneChanger = GameObject.Find("Scene Changer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        sceneChanger.GetComponent<Animator>().SetTrigger("nextLevel");
+    }
+}
