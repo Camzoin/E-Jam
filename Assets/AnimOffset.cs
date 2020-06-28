e6470010cf9de8657573056c711d4d3d3d63fdff
Camzoin <Camden.Cecrle@gmail.com>
2020-06-28 14:03:54 -0700

    Added talking anim

--- Assets/AnimOffset.cs
+++ Assets/AnimOffset.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class AnimOffset : MonoBehaviour
 {
+    float time = 0;
+    float timer = 1;
+    Animator thisAnim;
+
     // Start is called before the first frame update
     void Start()
     {
         Animator anim = GetComponent<Animator>();
         AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
         anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+
+        timer = Random.Range(0, 8);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        thisAnim = gameObject.GetComponent<Animator>();
+
+
+        time = time + Time.deltaTime;
+
+        if (time > timer)
+        {
+            float chance = Random.Range(0, 10);
+
+            Debug.Log("time but no trig" + chance);
+
+            if (chance >= 8)
+            {
+               thisAnim.SetTrigger("talk");
+               Debug.Log("tiggered");
+            }
+
+            timer = Random.Range(4, 8);
+            time = 0;
+        }
     }
 }
