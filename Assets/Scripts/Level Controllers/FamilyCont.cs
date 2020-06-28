dcd91e719bd16b5d1133a74837c209e9c3ba1a55
Camzoin <Camden.Cecrle@gmail.com>
2020-06-27 21:55:07 -0700

    College works

--- Assets/Scripts/Level Controllers/FamilyCont.cs
+++ Assets/Scripts/Level Controllers/FamilyCont.cs
@@ -19,6 +19,8 @@ public class FamilyCont : MonoBehaviour
 
     public bool interacted = false;
 
+    public GameObject sparkler;
+
     GameObject sceneChanger;
 
     // Start is called before the first frame update
