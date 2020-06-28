dcaba78f7058f92eb08e6157841d62455e879f78
Camzoin <Camden.Cecrle@gmail.com>
2020-06-28 13:29:14 -0700

    Added credits

---
+++ Assets/CredCont.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CredCont : MonoBehaviour
+{
+    public float timeToEvent = 30;
+
+    float timeInLevel = 0;
+
+    GameObject sceneChanger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneChanger = GameObject.Find("Scene Changer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInLevel = timeInLevel + Time.deltaTime;
+
+        if (timeInLevel > timeToEvent)
+        {
+            sceneChanger.GetComponent<Animator>().SetTrigger("nextLevel");
+        }
+    }
+}
