c61171e8fcc8cbabd153bb2f97d25a3409718772
Camzoin <Camden.Cecrle@gmail.com>
2020-06-27 21:50:37 -0700

    Big sparkler update

---
+++ Assets/FlickerLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    float time;
+
+    float timer = .2f;
+
+    public float baseIntensity = 0.005f;
+
+    float intensity = 0.005f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time = time + Time.deltaTime;
+
+        //if (time > timer)
+        //{
+        //    intensity = Mathf.Lerp(intensity, Random.Range(0.003f, 0.006f), 1 * Time.deltaTime);
+
+        //    time = 0;
+        //}
+
+        intensity = Mathf.Lerp(intensity, baseIntensity * Random.Range(0.5f, 1.5f), 1 * Time.deltaTime);
+
+        gameObject.GetComponent<Light>().intensity = intensity;
+    }
+}
