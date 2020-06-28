12859739821ead90f85c77077913f20a2eab24b2
Camzoin <Camden.Cecrle@gmail.com>
2020-06-28 10:57:59 -0700

    Scene 3 done

---
+++ Assets/Scripts/Level Controllers/CollegeCont.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollegeCont : MonoBehaviour
+{
+    public float timeToEvent = 30;
+
+    public PlayerMovement playerMove;
+    public MouseLook playerLook;
+
+    float timeInLevel = 0;
+
+
+    bool hasHappened = false;
+
+    public bool interacted = false;
+
+
+    GameObject sceneChanger;
+
+
+    public GameObject vommer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneChanger = GameObject.Find("Scene Changer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInLevel = timeInLevel + Time.deltaTime;
+
+        if (timeInLevel > timeToEvent)
+        {
+            if (hasHappened == false)
+            {
+                //event
+
+                //open door
+                vommer.GetComponent<Vommer>().isVom = true;
+
+                //voice line
+
+                //spawn sparkler box
+
+                hasHappened = true;
+            }
+            //wait for palyer to get sparkler
+
+            if (interacted == true)
+            {
+                //player get sparkler
+
+                //take controller from player
+
+                playerMove.playerControl = false;
+
+
+               //play fade to black anim
+               sceneChanger.GetComponent<Animator>().SetTrigger("nextLevel");
+                    
+                
+            }
+        }
+    }
+}
---
+++ Assets/Vommer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vommer : MonoBehaviour
+{
+    public bool isVom = false;
+
+    Animator character;
+
+    Transform player;
+
+    float interactRange = 3;
+
+    public GameObject vomSound, pressE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = transform.GetComponentInChildren<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isVom == true)
+        {
+            gameObject.GetComponent<Walker>().enabled = false;
+            character.SetTrigger("vom");
+
+            vomSound.SetActive(true);
+            pressE.SetActive(true);
+
+            if (Input.GetKeyDown("e"))
+            {
+                if (Vector3.Distance(player.position, transform.position) < interactRange)
+                {
+                    //set interacted
+
+                    GameObject controller = GameObject.FindGameObjectWithTag("GameController");
+
+                    controller.GetComponent<CollegeCont>().interacted = true;
+                }
+            }
+        }
+    }
+}
